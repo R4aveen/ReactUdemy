702cf4c47297dcc7c454b31ace1894ded9427318
r4veeen <bas.rojasl@duocuc.cl>
2024-07-07 21:48:26 -0400

    voy en los hooks

---
+++ src/HelloWorldApp.jsx
@@ -0,0 +1,18 @@
+// function App(){
+//     //document.createElement,....
+//     return (<h1>Hola mundo!!!!</h1>);
+// }
+
+// export default App;
+
+
+// export const HelloWorldApp = () => <h1>Arrow fuction</h1>;
+
+export const HelloWorldApp = () => {
+    
+  return (
+    <h1>Hello World App</h1>
+  )
+}
+
+
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import { HelloWorldApp } from './HelloWorldApp';
+// import { FirtsApp } from './FirstApp';
+import { CounterApp } from './CounterApp';
+
+import './style.css';
+
+ReactDOM.createRoot( document.getElementById('root')).render(
+    <React.StrictMode>
+        {/* <FirtsApp/> */}
+        <CounterApp value ={20}/>
+    </React.StrictMode>
+)
